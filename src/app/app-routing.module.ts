64c318363d573bf924940cb718fdf6abdf1984a5
ZiqSteven <57764915+ZiqSteven@users.noreply.github.com>
2020-10-27 01:10:12 -0500

    Main Page Finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { PoliticaComponent } from './components/politica/politica.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'politica-de-privacidad', component: PoliticaComponent},
+  { path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PoliticaComponent } from './components/politica/politica.component';
+import { Page404Component } from './components/page404/page404.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PoliticaComponent,
+    Page404Component,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
