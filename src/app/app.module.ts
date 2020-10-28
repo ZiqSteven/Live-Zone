e21e07d628f777f192e035297694f0b1c363c021
ZiqSteven <57764915+ZiqSteven@users.noreply.github.com>
2020-10-28 16:23:10 -0500

    Google log finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { StreamerDashComponent } from './components/streamer-dash/streamer-dash.component';
 import { FormsModule } from '@angular/forms';
 
+//Modulos para inicio de sesion con social login
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,9 +29,27 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('808131090708-5espkbnhf76qtdp160jl74q9vpbtejo8.apps.googleusercontent.com'),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('clientId'),
+          }
+        ],
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,Validators } from '@angular/forms';
 import { ToastrManager } from 'ng6-toastr-notifications';
 
-
-declare var FB: any;
+import { SocialAuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider, VKLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'app-home',
@@ -12,67 +12,32 @@ declare var FB: any;
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private fb:FormBuilder, public toastr: ToastrManager) { }
+  user: SocialUser;
+  loggedIn: boolean;
 
-  userInfo=this.fb.group({
-    userName:['',Validators.compose([Validators.required, Validators.pattern('[A-Za-z0-9]*')])],
-    password:['',Validators.compose([Validators.required, Validators.pattern('[A-Za-z0-9]*')])]
-  })
-
-  ngOnInit(): void {
-    (window as any).fbAsyncInit = function() {
-      FB.init({
-        appId      : '475900056660990',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v1.0'
-      });
-        
-      FB.AppEvents.logPageView();   
-        
-    };
-
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+  constructor(private authService: SocialAuthService){
 
   }
 
-  submitLogin(){
-    console.log("submit login to facebook");
-    FB.login((response)=>
-    {
-      console.log('submitLogin', response);
-      if(response.authResponse){
-        this.toastr.successToastr('login succesfull', 'Success!');
-      }else{
-        this.toastr.errorToastr('Login failed', 'Oops!');
-      }
+  ngOnInit(): void {
+    //esto es un observable que recibir data de manera asincrono y varias veces
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(user);
+      this.loggedIn = (user != null);
     });
   }
 
-  login() {
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
  
-    window['FB'].login((response) => {
-        console.log('login response',response);
-        if (response.authResponse) {
- 
-          window['FB'].api('/me', {
-            fields: 'last_name, first_name, email'
-          }, (userInfo) => {
- 
-            console.log("user information");
-            console.log(userInfo);
-          });
-           
-        } else {
-          console.log('User login failed');
-        }
-    }, {scope: 'email'});
-}
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
 
 }
