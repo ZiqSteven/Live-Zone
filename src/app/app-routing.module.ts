ad147dd92bdad7e33cf4071a14d56a838a52c437
ZiqSteven <57764915+ZiqSteven@users.noreply.github.com>
2020-10-28 19:01:49 -0500

    Renovando

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { StreamerDashComponent } from './components/streamer-dash/streamer-dash.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { PoliticaComponent } from './components/politica/politica.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'politica-de-privacidad', component: PoliticaComponent},
+  { path: 'streamer', component: StreamerDashComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PoliticaComponent } from './components/politica/politica.component';
+import { Page404Component } from './components/page404/page404.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StreamerDashComponent } from './components/streamer-dash/streamer-dash.component';
+import { FormsModule } from '@angular/forms';
+
+//Modulos para inicio de sesion con social login
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PoliticaComponent,
+    Page404Component,
+    NavbarComponent,
+    StreamerDashComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('808131090708-5espkbnhf76qtdp160jl74q9vpbtejo8.apps.googleusercontent.com'),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('475900056660990'),
+          }
+        ],
+      } as SocialAuthServiceConfig,
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
